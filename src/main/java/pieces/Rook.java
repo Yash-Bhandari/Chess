414a4636aa87c4848221996cfec92df8d57239b2
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2019-05-21 20:06:23 -0600

    added rook movement

--- src/main/java/pieces/Rook.java
+++ src/main/java/pieces/Rook.java
@@ -3,6 +3,7 @@ package pieces;
 import board.ChessBoard;
 import helper.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Rook extends Piece {
@@ -11,7 +12,29 @@ public final class Rook extends Piece {
     }
 
     @Override
-    public List<Position> getMoves(ChessBoard board, Position selfPos) {
-        return null;
+    public List<Position> getMoves(ChessBoard board, Position p) {
+        List<Position> validMoves = new ArrayList<>();
+
+        int up = 1;
+        while (board.validSpot(p.add(-up, 0), getTeam())) {
+            validMoves.add(p.add(-up++, 0));
+        }
+
+        int down = 1;
+        while (board.validSpot(p.add(down, 0), getTeam())) {
+            validMoves.add(p.add(down++, 0));
+        }
+
+        int right = 1;
+        while (board.validSpot(p.add(0, right), getTeam())) {
+            validMoves.add(p.add(0, right++));
+        }
+
+        int left = 1;
+        while (board.validSpot(p.add(0, -left), getTeam())) {
+            validMoves.add(p.add(0, -left++));
+        }
+
+        return validMoves;
     }
 }
