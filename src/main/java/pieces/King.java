2dec059399dd360c0300f83854c0775a95b55285
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2019-05-23 20:20:44 -0600

    working on castling

--- src/main/java/pieces/King.java
+++ src/main/java/pieces/King.java
@@ -26,10 +26,28 @@ public final class King extends Piece {
         validMoves.add(p.add(0, -1));
 
 
+        validMoves.addAll(castle(board, p));
 
         pruneMoves(validMoves, board, p);
 
         return validMoves;
     }
+
+    // TODO
+    private List<Position> castle(ChessBoard board, Position p){
+        ArrayList<Position> validMoves = new ArrayList<>();
+        if (hasItMoved())
+            return validMoves;
+
+        Piece leftRook = board.pieceAt(new Position(getTeam() == 0 ? 7 : 7, 0));
+        Piece rightRook = board.pieceAt(new Position(getTeam() == 0 ? 7 : 7, 0));
+
+        if (leftRook != null && leftRook.getType() == PieceType.ROOK && !leftRook.hasItMoved()) {
+            boolean blocked = false;
+            //or (int i = 1; i < 4)
+        }
+        return validMoves;
+    }
+
 
 }
